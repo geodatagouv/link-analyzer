6e2a2848e9bed3798107d7a9404bc1056889fa7f
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-08-15 21:25:59 +0200

    chore: move api stuff in their own directory

--- web.js
+++ web.js
@@ -7,8 +7,7 @@ const morgan = require('morgan')
 
 require('./lib/mongoose')
 
-const { authenticateSubscriber } = require('./lib/middlewares')
-const linksController = require('./lib/controllers/links')
+const api = require('./lib/api')
 const config = require('./lib/config')
 
 const app = express()
@@ -17,17 +16,6 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(morgan('dev'))
 
-/* Injectors */
-app.param('linkId', linksController.link)
-app.param('checkNum', linksController.check)
-
-/* Actions */
-app.get('/api/links/last-created', linksController.lastCreated)
-app.get('/api/links/:linkId', linksController.show)
-app.post('/api/links/:linkId/checks', linksController.doCheck)
-app.get('/api/links/:linkId/checks', linksController.checks)
-app.get('/api/links/:linkId/checks/last', linksController.lastCheck)
-app.get('/api/links/:linkId/checks/:checkNum', linksController.showCheck)
-app.post('/api/links', authenticateSubscriber, linksController.upsertAndSubscribe)
+app.use('/api', api)
 
 app.listen(config.get('port'))
